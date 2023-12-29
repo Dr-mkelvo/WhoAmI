e51219ce9071fd16a5698a8cd8ad8b092c98c6ea
Mwiyeria Kelvin <118157498+Dr-mkelvo@users.noreply.github.com>
2023-12-29 19:58:24 +0300

    Add files via upload
    First commit

---
+++ components/Main.js
@@ -0,0 +1,19 @@
+import React from "react"
+
+export default function Main() {
+    return (
+        <main>
+            <h1 className="main--title">Fun facts about Mkelvo</h1>
+            <ul className="main--facts">
+                <li>He is a software Engineer</li>
+                <li>He is an Analytical Chemist </li>
+                <li>He is a Model</li>
+                <li>He loves swimming and travelling</li>
+                
+                                
+                
+                
+                </ul>
+        </main>
+    )
+}
